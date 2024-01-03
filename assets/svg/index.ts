341903994ecc60936953a18955e97b5c59c413d6
Shane Keney <sckeney@gmail.com>
2024-01-02 22:39:09 -0500

    feat: added svg assets for ui designs

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,9 +1,31 @@
-import Headphones from "./headphones.svg";
+import ArrowRight from './arrow-right.svg';
+import Bookmark from './bookmark.svg';
+import Clock from './clock.svg';
+import Comments from './comments.svg';
+import Discover from './discover.svg';
+import Home from './home.svg';
+import Like from './like.svg';
+import MagnifyGlass from './magnify-glass.svg';
+import Playlist from './playlist.svg';
+import Profile from './profile.svg';
+import Plus from './plus.svg';
+import Share from './share.svg';
 
 // Add SVG components here.
 // Key will dictate how to use the component i.e. <SVG.* /> where * is the key.
 export const SVG = {
-  Headphones,
+  ArrowRight,
+  Bookmark,
+  Clock,
+  Comments,
+  Discover,
+  Home,
+  Like,
+  MagnifyGlass,
+  Playlist,
+  Profile,
+  Plus,
+  Share
 } as const;
 
 export default SVG;
